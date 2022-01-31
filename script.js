ea62890b893af0ab0c4ba3277123c5b3ac707827
Daprojects1 <davidrtm45@gmail.com>
2022-01-31 13:25:47 +0000

    Almost completed functionality in the first page

---
+++ script.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav")
+// when window size chnages, it adds or remove the bootstrap class that expands the navigation
+window.addEventListener("resize", ()=>{
+    if (window.innerWidth > 768){
+        nav.classList.add("navbar-expand")
+    }else{
+        nav.classList.remove("navbar-expand")
+    }
+})
