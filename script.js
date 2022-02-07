82e8a055761c58f370b563a617468e637570cf77
Daprojects1 <davidrtm45@gmail.com>
2022-02-07 13:51:24 +0000

    First complete draft

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ window.addEventListener("resize", ()=>{
         nav.classList.remove("navbar-expand")
     }
 })
+// icons 
+let footIcon = document.querySelector(".ft-icon")
+let footIcon1 = document.querySelector(".ft-icon1")
+let footIcon2 = document.querySelector(".ft-icon2")
+let footIcon3 = document.querySelector(".ft-icon3")
+
+// unordered list
+let footList = document.querySelector(".foot-ul")
+let footList1 = document.querySelector(".foot-ul1")
+let footList2 = document.querySelector(".foot-ul2")
+let footList3 = document.querySelector(".foot-ul3")
+
+let mainArr = [footIcon, footIcon1, footIcon2, footIcon3]
+
+const mainFunc = ()=>{
+ mainArr.forEach((item, array)=>{
+   item.addEventListener("click",()=>{
+    if (array=== 0){
+        footIcon.classList.toggle("rotate-icon")
+        footList.classList.toggle("show")
+    } else if (array === 1){
+        footIcon1.classList.toggle("rotate-icon")
+        footList1.classList.toggle("show")
+    }else if (array === 2){
+        footIcon2.classList.toggle("rotate-icon")
+        footList2.classList.toggle("show")
+    }else if (array === 3){
+        footIcon3.classList.toggle("rotate-icon")
+        footList3.classList.toggle("show")
+    } 
+   })
+    })
+}
+
+mainFunc();
+
